b51b7db29bf908a332620ff8ba9c8d6ecee5fe2d
Rocky Mor <rockymor2003@gmail.com>
2024-06-12 07:03:40 +0530

    done some variable practice

---
+++ 00. Practice/Index.java
@@ -0,0 +1,22 @@
+
+
+/**
+ * Index
+ */
+public class Index {
+  static int a = 2;
+
+  int b = 3;
+
+  public static void main(String[] args) {
+    int x = 4;
+
+    Index v = new Index();
+
+   System.out.println(v.b);
+   System.out.println(a);
+   System.out.println(x);
+
+  //  System.out.print(v.b+'-'+a+'-'+x);
+  }
+}
